6d4c206dc0d2c8d19556b475c03e526129f88b1a
adolorfi <adolorfi@calpoly.edu>
2022-02-15 11:32:14 -0800

    annotated work

--- src/main.py
+++ src/main.py
@@ -1,3 +1,15 @@
+"""!
+    @file       user_interface.py
+        This file is responsible for collecting collected from the RC
+        circuit. It communicates with the MicroPython directly and sends data
+        to the VCP to be read and plotted separately.
+
+    @author: Chloe Chou
+    @author: Aleya Dolorfino
+    @author: Christian Roberts
+    @date: February 15, 2022
+"""
+
 import pyb
 import micropython
 import utime
@@ -6,38 +18,62 @@ from pyb import USB_VCP
 vcp = USB_VCP()
 
 micropython.alloc_emergency_exception_buf(100)
+
+# Creates a queue to hold 1000 16-bit integers
 my_queue = task_share.Queue('H', 1000)
 
+
 def handler(tim):
+     """!
+        This method reads values off the circuit and store them in a queue
+        to be read separately.
+        
+        @param tim A timer object from the MicroPython board.
+        """
     x = ADC_C0.read()
     my_queue.put(x)
     
 
 if __name__ == '__main__':
+
     while True:
         if vcp.any():
+            # Creates pins as an input and output
             pinC1 = pyb.Pin(pyb.Pin.board.PC1, mode = pyb.Pin.OUT_PP)
             pinC0 = pyb.Pin(pyb.Pin.board.PC0, mode = pyb.Pin.IN)
             ADC_C0 = pyb.ADC(pinC0)
+            
+            # Creates the time and voltage arrays to store the information
             time = []
             voltage = []
+            
+            #Keep the output pin low to begin with
             pinC1.low()
             
+            #Creates a timer object to track the data
             tim = pyb.Timer(1, freq = 1000)
+            
+            #Turn the pin on
             pinC1.high()
             
+            # Wait 1000 ms
             tim.callback(handler)
             utime.sleep_ms(1000)
             tim.callback(None)
             
+            # Append the queue data into the established arrays
             print("printing queue")
             for x in range (my_queue.num_in()):
                 time.append(x)
                 voltage.append(my_queue.get())
+            
+            # Turn the pin off after the data is collected
             pinC1.low()
             
+            #Clear the queue
             my_queue.clear()
             
+            # Print the data into the VCP
             state = 0
             while True:
                 if state == 0:
--- src/user_interface.py
+++ src/user_interface.py
@@ -1,33 +1,40 @@
 """!
     @file       user_interface.py
-        This file is responsible for the user interface of the motor commands. It
-        communicates with the computer directly and takes motor's output data and
-        plots it in a graph when the enter key on the keyboard is pressed.
+        This file communicates with the computer directly and is responsible
+        for interfacing with the user. When the enter key on the keyboard is
+        pressed, it plots the data collected from the RC circuit in a graph.
 
     @author: Chloe Chou
     @author: Aleya Dolorfino
     @author: Christian Roberts
-    @date: February 8, 2022
+    @date: February 15, 2022
 """
 from matplotlib import pyplot 
 import serial
 
+#Sets up connection from computer on COM3
 ser_port = serial.Serial("COM3", 115200, timeout = 10)
 
+#Establishes data sets for x and y axes to be filled with data
 x_axis = []
 y_axis = []
 
+#Establishes different states: init state, waiting for character, append, and plot.
 S0_INIT = 0
 S1_WAIT_FOR_CHAR = 1
 S2_APPEND = 2
 S3_PLOT = 3
 
+#Begins at State 0 
 state = S0_INIT
 while True:
+    
+    #State 0 prints welcome message
     if state == S0_INIT:
         print ('\033[2JWelcome to ME 405 Lab 4\r\n')
         state = S1_WAIT_FOR_CHAR
-        
+    
+    #State 1 prints message for users
     elif state == S1_WAIT_FOR_CHAR:
         char_in = input("Press ENTER to run the step response:")
         ser_port.write(b'g')
@@ -40,6 +47,8 @@ while True:
             except IndexError:
                 print('index')
                 pass
+            
+    #State 2 appends data from circuit into the x and y axes arrays
     elif state == S2_APPEND:
         if ser_port.in_waiting>0:
                 if ser_port.in_waiting != 0:
@@ -58,7 +67,7 @@ while True:
                         pass
         else:
             pass
-        
+    # State 3 plots all data into a graph
     elif state == S3_PLOT:
         
         fig = pyplot.figure()
